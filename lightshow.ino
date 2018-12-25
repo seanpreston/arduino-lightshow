c16c136b30cf4eda0ebc1f20117c09e4d413f733
Sean Preston <seanmpreston@gmail.com>
2018-12-24 18:56:05 -0700

    add back in fastled

--- lightshow.ino
+++ lightshow.ino
@@ -1,4 +1,5 @@
 #include "IRremote.h"
+#include <FastLED.h>
 
 int receiver = 11; // Signal Pin of IR receiver to Arduino Digital Pin 11
 
@@ -23,30 +24,30 @@ void loop() {
 
 
 void translateIR() {
-  Serial.println("Results are: ");
-  Serial.println(results.value);
+  // Serial.println("Results are: ");
+  // Serial.println(results.value);
 
-  // switch(results.value) {
-  //   case 0xFF629D: Serial.println(" FORWARD"); break;
-  //   case 0xFF22DD: Serial.println(" LEFT");    break;
-  //   case 0xFF02FD: Serial.println(" -OK-");    break;
-  //   case 0xFFC23D: Serial.println(" RIGHT");   break;
-  //   case 0xFFA857: Serial.println(" REVERSE"); break;
-  //   case 0xFF6897: Serial.println(" 1");    break;
-  //   case 0xFF9867: Serial.println(" 2");    break;
-  //   case 0xFFB04F: Serial.println(" 3");    break;
-  //   case 0xFF30CF: Serial.println(" 4");    break;
-  //   case 0xFF18E7: Serial.println(" 5");    break;
-  //   case 0xFF7A85: Serial.println(" 6");    break;
-  //   case 0xFF10EF: Serial.println(" 7");    break;
-  //   case 0xFF38C7: Serial.println(" 8");    break;
-  //   case 0xFF5AA5: Serial.println(" 9");    break;
-  //   case 0xFF42BD: Serial.println(" *");    break;
-  //   case 0xFF4AB5: Serial.println(" 0");    break;
-  //   case 0xFF52AD: Serial.println(" #");    break;
-  //   case 0xFFFFFFFF: Serial.println(" REPEAT");break;
-  //
-  //   default:
-  //     Serial.println(" other button   ");
-  // }
+  switch(results.value) {
+    case 0xFF629D: Serial.println(" FORWARD"); break;
+    case 0xFF22DD: Serial.println(" LEFT");    break;
+    case 0xFF02FD: Serial.println(" -OK-");    break;
+    case 0xFFC23D: Serial.println(" RIGHT");   break;
+    case 0xFFA857: Serial.println(" REVERSE"); break;
+    case 0xFF6897: Serial.println(" 1");    break;
+    case 0xFF9867: Serial.println(" 2");    break;
+    case 0xFFB04F: Serial.println(" 3");    break;
+    case 0xFF30CF: Serial.println(" 4");    break;
+    case 0xFF18E7: Serial.println(" 5");    break;
+    case 0xFF7A85: Serial.println(" 6");    break;
+    case 0xFF10EF: Serial.println(" 7");    break;
+    case 0xFF38C7: Serial.println(" 8");    break;
+    case 0xFF5AA5: Serial.println(" 9");    break;
+    case 0xFF42BD: Serial.println(" *");    break;
+    case 0xFF4AB5: Serial.println(" 0");    break;
+    case 0xFF52AD: Serial.println(" #");    break;
+    case 0xFFFFFFFF: Serial.println(" REPEAT");break;
+
+    default:
+      Serial.println(" other button   ");
+  }
 }
